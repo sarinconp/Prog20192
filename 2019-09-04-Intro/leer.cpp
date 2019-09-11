64d2e9bd95f83206076361e4e09be479a3ca5259
Iluvatar <woquendo@gmail.com>
2019-09-11 08:32:35 -0500

    Added example to use cin

---
+++ 2019-09-04-Intro/leer.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+
+int main(void)
+{
+  double x = 0.0;
+  std::cout << "Escribe el numero :\n" ;
+  std::cin >> x;
+  std::cout << " He leido : " << x << "\n"; 
+  
+  return 0;
+}
