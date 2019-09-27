fc87f0d409bc121857ef06427713e9740b741519
Iluvatar <woquendo@gmail.com>
2019-09-27 07:26:47 -0500

    Added overloaded function with double

--- 2019-09-27-Functions-II/max.cpp
+++ 2019-09-27-Functions-II/max.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
 int max(int a, int b);
+double max(double a, double b); // overloaded function
 
 int main(void)
 {
-  std::cout << max(-3, 2) << "\n";
+  std::cout << max(-3.2, -3.5) << "\n";
+  std::cout << max(2, -5) << "\n";
+  //std::cout << max(1.1, -7) << "\n"; // error, no hay funcion sobre cargada para esta combinacion
   
   return 0;
 }
@@ -12,6 +15,18 @@ int main(void)
 int max(int a, int b)
 {
   int result = 0;
+  if ( a >= b ) {
+    result = a;
+  }
+  else {
+    result = b;
+  }
+  return result;
+}
+
+double max(double a, double b)
+{
+  double result = 0;
   if ( a >= b ) {
     result = a;
   }
